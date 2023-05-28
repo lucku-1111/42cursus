b1716b53ba51f21e0cb975ebed477854a0f1d390
lucku <lsw6731@naver.com>
2023-05-28 20:51:20 +0900

    philosophers

---
+++ philosopher/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seoklee <seoklee@student.42.kr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 18:16:06 by seoklee           #+#    #+#             */
+/*   Updated: 2023/05/28 20:19:15 by seoklee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	t_info	info;
+
+	if (!init_info(&info, ac, av))
+		return (print_err("Init fail."));
+	if (!ft_threads(info))
+		return (print_err("Ft_threads fail."));
+}
